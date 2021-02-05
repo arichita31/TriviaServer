7b15fcd131cda258723e1c7937ca5d7535cdceb9
Ariel Chitiat <arielchitiat@gmail.com>
2021-02-05 12:25:09 +0200

    add chatlib and client side

---
+++ chatlib.py
@@ -0,0 +1,207 @@
+import socket
+
+# Protocol Constants
+
+CMD_FIELD_LENGTH = 16  # Exact length of cmd field (in bytes)
+LENGTH_FIELD_LENGTH = 4  # Exact length of length field (in bytes)
+MAX_DATA_LENGTH = 10 ** LENGTH_FIELD_LENGTH - 1  # Max size of data field according to protocol
+MSG_HEADER_LENGTH = CMD_FIELD_LENGTH + 1 + LENGTH_FIELD_LENGTH + 1  # Exact size of header (CMD+LENGTH fields)
+MAX_MSG_LENGTH = MSG_HEADER_LENGTH + MAX_DATA_LENGTH  # Max size of total message
+DELIMITER = "|"  # Delimiter character in protocol
+
+# Protocol Messages
+# In this dictionary we will have all the client and server command names
+
+#לבדוק למה צריך את זה את המילון הזה אם אין שימוש בצד שמאל שלו בכלל
+PROTOCOL_CLIENT = {
+    "login_msg": "LOGIN",
+    "logout_msg": "LOGOUT",
+    "logged_msg": "LOGGED",
+    "get_question_msg": "GET_QUESTION",
+    "send_answer_msg": "SEND_ANSWER",
+    "my_score_msg": "MY_SCORE",
+    "high_score_msg": "HIGH_SCORE",
+
+}  # .. Add more commands if needed
+
+PROTOCOL_SERVER = {
+    "login_ok_msg": "LOGIN_OK",
+    "login_failed_msg": "ERROR"
+}  # ..  Add more commands if needed
+
+# Other constants
+
+ERROR_RETURN = None  # What is returned in case of an error
+
+
+# def is_msg_valid(msg):
+#     msg_parts = msg.split(DELIMITER)
+#     if msg_parts[0].length != CMD_FIELD_LENGTH:
+#         return False
+#     if msg_parts[1].length != LENGTH_FIELD_LENGTH or msg_parts[1].isnumeric() == False or int(msg_parts[1]) < 0:
+#         return False
+#     if msg_parts[2].length != int(msg_parts[1]):
+#         return False
+#     return True
+
+def is_parameters_valid(command, data):
+    """
+
+    :param command_code:
+    :param data:
+    :return:
+    """
+    # check if the command_code is string
+    if not isinstance(command, str):
+        return False
+
+    # check if the data is string
+    if not isinstance(data, str):
+        return False
+
+    # check if the command name is in the commands dictionary values
+    command_found = False
+    for key in PROTOCOL_CLIENT.keys():
+        if PROTOCOL_CLIENT[key] == command.strip():
+            command_found = True
+            break
+
+    if not command_found:
+        for key in PROTOCOL_SERVER.keys():
+            if PROTOCOL_SERVER[key] == command.strip():
+                command_found = True
+                break
+
+    if not command_found:
+        return False
+
+    # check if the data len is no more than the limit
+    if len(data) > 9999:
+        return False
+
+    return True
+
+
+def build_message(command_code, data): #לבדוק עם צביקה מה החוקים של הפרוטוקול
+    """
+    Gets command name and data field and creates a valid protocol message
+    Returns: str, or None if error occured
+    """
+    # check if parameters sre valid
+    if not is_parameters_valid(command_code, data):
+        print("Values are not valid!")
+        return ERROR_RETURN
+
+    # first msg part
+    command = command_code.ljust(16)
+    # second msg part
+    length_msg = str(len(data))
+    length_msg = length_msg.rjust(4, '0')
+    # third msg part is data
+    message_fields = [command, length_msg, data]
+
+    return join_msg(message_fields)
+
+
+def is_fields_valid(command, length, data): # לבדוק אם להכניס 0 או רווח אם צריך להשלים את שדה האורך של ההודעה,
+    """
+
+    :param command: the command name
+    :param length:
+    :param data:
+    :return:
+    """
+    # check if the command field length is ok
+    if len(command) != CMD_FIELD_LENGTH:
+        return False
+
+    # check if the length field length is ok
+    if len(length) != LENGTH_FIELD_LENGTH:
+        return False
+
+    # check if the command name is in the commands dictionary values
+    command_found = False
+    for key in PROTOCOL_CLIENT.keys():
+        if PROTOCOL_CLIENT[key] == command.strip():
+            command_found = True
+            break
+
+    if not command_found:
+        for key in PROTOCOL_SERVER.keys():
+            if PROTOCOL_SERVER[key] == command.strip():
+                command_found = True
+                break
+
+    if not command_found:
+        return False
+
+    # check if the length is a number
+    if not length.strip().isnumeric():
+        return False
+
+    # check if the data length is equal to length
+    if int(length.strip()) != len(data):
+        return False
+
+    # check if the data len is no more than the limit
+    if int(length.strip()) > MAX_DATA_LENGTH:
+        return False
+
+    return True
+
+
+def parse_message(data): # לבדוק אם ניתן לקבל יותר מ3 מפרידים מסוג |
+    """
+    Parses protocol message and returns command name and data field
+    Returns: cmd (str), data (str). If some error occured returns None, None
+    """
+    # split the message to its parts
+    msg_fields = split_msg(data)
+
+    # check if we got values for the message parts
+    for field in msg_fields:
+        if field is None:
+            return ERROR_RETURN, ERROR_RETURN
+
+    # the command id the first field
+    command = msg_fields[0]
+    # the length is the second field
+    length = msg_fields[1]
+    # the data is the third field
+    data = msg_fields[2]
+
+    # check if the command that was given is written right
+    if not is_fields_valid(command, length, data):
+        print("Not Valid!")
+        return ERROR_RETURN, ERROR_RETURN
+
+    command_name = command.strip()
+
+    return command_name, data
+
+
+def split_msg(msg, expected_fields=3):
+    """
+    Helper method. gets a string and number of expected fields in it. Splits the string
+    using protocol's delimiter (|) and validates that there are correct number of fields.
+    Returns: list of fields if all ok. If some error occured, returns None
+    """
+    msg_fields = msg.split(DELIMITER)
+
+    # check if the count of the fields is matching the expected
+    if len(msg_fields) != expected_fields:
+        return ERROR_RETURN, ERROR_RETURN
+
+    return msg_fields
+
+
+def join_msg(msg_fields):
+    """
+    Helper method. Gets a list, joins all of it's fields to one string divided by the delimiter.
+    Returns: string that looks like cell1|cell2|cell3
+    """
+    return DELIMITER.join(msg_fields)
+
+
+
+
--- client.py
+++ client.py
@@ -1,7 +1,131 @@
 import socket
+import chatlib  # To use chatlib functions or consts, use chatlib.****
 
-IP, PORT = "127.0.0.1", 8080
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((IP, PORT))
+SERVER_IP = "127.0.0.1"  # Our server will run on same computer as client
+SERVER_PORT = 5678
 
-sock.send("hello world".encode())
+
+# HELPER SOCKET METHODS
+
+def build_and_send_message(conn, command_code, data):
+    """
+    Builds a new message using chatlib, wanted code and message.
+    Prints debug info, then sends it to the given socket.
+    Paramaters: conn (socket object), code (str), msg (str)
+    Returns: nothing
+    """
+    message = chatlib.build_message(command_code, data)
+    conn.send(message.encode())
+    # Debug Info
+    print("-----------------------------")
+    print("We Sent The Server: \n" + message)
+
+
+
+
+def recv_message_and_parse(conn):
+    """
+    Recieves a new message from given socket.
+    Prints debug info, then parses the message using chatlib.
+    Paramaters: conn (socket object)
+    Returns: cmd (str) and data (str) of the received message.
+    If error occured, will return None, None
+    """
+    message_gotten = conn.recv(16384).decode()
+    # Debug Info
+    print("-----------------------------")
+    print("The Server Sent Us: \n" + message_gotten)
+    # parse the message using chatlib
+    command_code, msg_data = chatlib.parse_message(message_gotten)
+    return command_code, msg_data
+
+
+def connect():
+    """
+    creates a socket for the client for passing messages to the server
+    :return: socket connected to the Trivia server
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER_IP, SERVER_PORT))
+
+    return sock
+
+
+def error_and_exit(msg):
+    """
+    when error occurs the function finishes the program and prints the error message
+    :param msg: error message
+    :return: nothing
+    """
+    print(msg)
+    exit()
+
+
+
+def login(conn):
+    """
+    the function gets username and password builds a message according to the Protocol. tries to login, while login don't it requests again
+    :param conn - socket connected to server:
+    :return - finishes function:
+    """
+    # loop - as login failed asks again for username and password
+    while True:
+        # input from client
+        print("-----------------------------")
+        username = input("Please enter username: \n")
+        print("-----------------------------")
+        password = input("Please enter password: \n")
+        # create the data for login message
+        message_data = username + "#" + password
+        # build message using chatlib
+        build_and_send_message(conn, chatlib.PROTOCOL_CLIENT["login_msg"], message_data)
+        # Debug Info
+
+
+
+        # recieved answer from server
+        command_code, message_gotten_data = recv_message_and_parse(conn)
+
+        # cheking if login succeeded
+        if command_code == chatlib.PROTOCOL_SERVER["login_ok_msg"]:
+            print("-----------------------------")
+            print(f'Logged in as: {username} :)')
+            return
+        else:
+            print("-----------------------------")
+            print("Login failed :( \n Try Again!")
+
+
+
+
+def logout(conn):
+    """
+    function disconnects client from server
+    :param conn - socket connected to server:
+    :return:
+    """
+    # build the logout message and send the message to the server
+    build_and_send_message(conn, chatlib.PROTOCOL_CLIENT["logout_msg"], "")
+
+    print("-----------------------------")
+    print("Logged OUT! Take Care :)")
+
+
+
+
+def main():
+    """
+    connect socket to server then ask login then logout and finish
+    :return:
+    """
+    # connect socket to server
+    sock = connect()
+    # login your user
+    login(sock)
+    # logot from system
+    logout(sock)
+    # close the socket
+    sock.close()
+
+if __name__ == '__main__':
+    main()
