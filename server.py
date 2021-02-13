9cb361752af598e11bd8ccf6ba0fe2f35f1cecdf
yoav <yoavlevin10@gmail.com>
2021-02-13 16:20:02 +0200

    7 section 1, 2

--- server.py
+++ server.py
@@ -4,11 +4,13 @@
 
 import socket
 import chatlib
+import random
 
 # GLOBALS
 users = {}
 questions = {}
 logged_users = {}  # a dictionary of client hostnames to usernames - will be used later
+asked_questions = {}  # a dictionary with all the questions codes that the user (key) have been asked
 
 ERROR_MSG = "Error! "
 SERVER_PORT = 5678
@@ -72,9 +74,9 @@ def load_user_database():
     Returns: user dictionary
     """
     users = {
-        "test"	:	{"password": "test", "score": 0, "questions_asked": []},
-        "yossi"		:	{"password": "123", "score": 50, "questions_asked": []},
-        "master"	:	{"password": "master", "score": 200, "questions_asked": []}
+        "test": {"password": "test", "score": 0, "questions_asked": []},
+        "yossi": {"password": "123", "score": 50, "questions_asked": []},
+        "master": {"password": "master", "score": 200, "questions_asked": []}
     }
     return users
 
@@ -127,6 +129,7 @@ def handle_logout_message(conn):
     # close the socket of the client
     conn.close()
 
+
 # Implement code ...
 
 
@@ -138,7 +141,7 @@ def handle_login_message(conn, data):
     Returns: None (sends answer to client)
     """
     global users  # This is needed to access the same users dictionary from all functions
-    global logged_users	 # To be used later
+    global logged_users  # To be used later
     # extract the username and the password from the given data message
     givenuname, givenpass = data.split("#")
     # check if the user is exist in the dictionary and that its password matches the the password that was given
@@ -156,7 +159,7 @@ def handle_client_message(conn, msg_code, data):
     Recieves: socket, message code and data
     Returns: None
     """
-    global logged_users	 # To be used later
+    global logged_users  # To be used later
     # check if the client is logged
     is_logged = False
     if conn.getpeername() in logged_users.keys():
@@ -178,11 +181,66 @@ def handle_client_message(conn, msg_code, data):
         elif msg_code == chatlib.PROTOCOL_CLIENT["get_score_msg"]:
             handle_getscore_message(conn, logged_users[conn.getpeername()])
 
+        elif msg_code == chatlib.PROTOCOL_CLIENT["get_question_msg"]:
+            handle_question_message(conn, logged_users[conn.getpeername()])
+
         else:
             send_error(conn, "Error! The command is not recognized!")
 
     else:
         send_error(conn, "Error! You need to login first to have permission!")
+
+
+def create_random_question(username):
+    """
+       Return the question and answers in YOUR QUESTION format(only the data)
+       :param username: the name of the user
+       :return: the data for YOUR_QUESTION
+       """
+
+    questions_dict_len = len(questions)
+    key_list = list(questions.keys())
+    if username not in asked_questions.keys():  # if the username isn't save, create field for him
+        asked_questions[username] = []
+    while True:  # search random question that wasn't answered until you find one then return
+        random_question_num = random.randint(0, questions_dict_len - 1)
+        question_code = key_list[random_question_num]
+
+        if question_code not in asked_questions[username]:
+            # if the question wasn't asked return the question and answers
+            # add the question code to the questions that were asked
+            asked_questions[username].append(question_code)
+            # arrange the answer by the YOUR_QUESTION format
+
+            answers = questions[question_code]["answers"]
+            data_fields = [str(question_code), questions[question_code]["question"], answers[0], answers[1], answers[2],
+                           answers[3]]
+            data = "#".join(data_fields)
+            return data
+        # if the question was already asked check if all the questions
+        # were asked (by length) and return None if True
+        else:
+            if len(questions) == len(asked_questions[username]):
+                return None
+
+
+def handle_question_message(conn, username):
+    """
+    Send appropriate message: send question in Your_Question format
+    or send No_Questions message in the appropriate format
+
+    :param conn: the client socket
+    :param username: the name of the user
+    :return: nothing
+    """
+    data = create_random_question(username)
+    if data is not None:
+        # return the Your_Question message
+        msg_code = chatlib.PROTOCOL_SERVER["your_question_msg"]
+    else:
+        # return the No Questions message
+        msg_code = chatlib.PROTOCOL_SERVER["no_questions_msg"]
+    build_and_send_message(conn, msg_code, data)
 
 
 def main():
@@ -213,6 +271,7 @@ def main():
                 client_connected = False
 
     ############################## NEED TO FIX A MESSAGE WITH A COMMAND WHO ISNT RECOGNIZED
+
 
 if __name__ == '__main__':
     main()
